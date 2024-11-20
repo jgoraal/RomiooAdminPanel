4b7aa16b91eed6a9c5425c0f1ffd96727e723d4b
jgoraal <jakub.gorski@student.put.poznan.pl>
2024-11-20 19:45:41 +0100

    hahah

--- callbacks.py
+++ callbacks.py
@@ -101,7 +101,7 @@ def register_callbacks(app,users,reservations,rooms):
                     buttons=list([
                         dict(count=1, label='1m', step='month', stepmode='backward'),
                         dict(count=6, label='6m', step='month', stepmode='backward'),
-                        dict(count=1, label='YTD', step='year', stepmode='todate'),
+                        #dict(count=1, label='YTD', step='year', stepmode='todate'),
                         dict(count=1, label='1y', step='year', stepmode='backward'),
                         dict(step='all', label='Wszystko')
                     ])
